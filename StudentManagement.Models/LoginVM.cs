01e80bfbd75f77b4291d022a2855110b318217aa
sandeep kumar <sandeepkumar.sk5@outlook.com>
2024-03-14 00:29:52 +0530

    Created Login page with Enum Role

---
+++ StudentManagement.Models/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
+{
+    public class LoginVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        public int Role { get; set; }
+    }
+}
