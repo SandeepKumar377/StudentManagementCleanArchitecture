ce27bd7b9906b280fbd254cdd6561c1d75a1749b
sandeep kumar <sandeepkumar.sk5@outlook.com>
2024-03-14 00:09:09 +0530

    Created classes of GenericRepo and UnitOfWork

---
+++ StudentManagement.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using StudentManagement.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Data.UnitOfWork
+{
+    public class UnitOfWork(ApplicationDbContext context) : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public IGenericRepository<T> GenericRepository<T>() where T : class
+        {
+            IGenericRepository<T> genericRepo = new GenericRepository<T>(_context);
+            return genericRepo;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        #region IDisposable Members
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
